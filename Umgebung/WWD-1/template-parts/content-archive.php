b8016c6ee11da3d79b647ee1adda0c7e762c378c
WendlandWebDesign <wendlandwebdesign@gmail.com>
2025-07-17 09:37:22 +0200

    Delete Umgebung directory

--- Umgebung/WWD-1/template-parts/content-archive.php
+++
@@ -1,9 +0,0 @@
-
-
-
-        <?php 
-            the_excerpt();
-        ?>
-            <a class="more-link" href="<?php the_permalink(); ?>">Mehr Lesen</a>
-    </div>
-
--- Umgebung/WWD-1/template-parts/content-article.php
+++
@@ -1,4 +0,0 @@
-<img class="post-img" src="<?php the_post_thumbnail_url(); ?>">
-<?php 
-the_content();
-?>
